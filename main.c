a3ba22fa1db5a26ec9910889d6e2d26c485093e8
HaechangLee <lcidavid@kw.ac.kr>
2024-04-30 15:45:59 +0900

    impl of filetime

--- main.c
+++ main.c
@@ -31,21 +31,36 @@ int main(void)
 //파일 1의 정보를 가져오는 함수 작성
 void filestat1(void)
 {
+    if(stat("./text1", &stat1) == -1) {
+        perror("Error occurred while reading filestat of text2");
+        return;
+    }
+    // printf("%ld\n",stat1.st_size);
+
 }
 
 //파일 2의 정보를 가져오는 함수 작성
 void filestat2(void)
 {
+    if(stat("./text2", &stat2) == -1) {
+        perror("Error occurred while reading filestat of text2");
+        return;
+    }
+    // printf("%ld\n",stat2.st_size);
 }
 
 //파일 1의 시간 정보를 가져오는 함수 작성
 void filetime1(void)
 {
+    time1 = localtime(&stat1.st_mtime);
+    // printf("modified at %d/%d %d:%d\n", time1->tm_mon, time1-> tm_mday, time1->tm_hour,time1->tm_min);
 }
 
 //파일 2의 시간 정보를 가져오는 함수 작성
 void filetime2(void)
 {
+    time2 = localtime(&stat2.st_mtime);
+    // printf("modified at %d/%d %d:%d\n", time2->tm_mon, time2-> tm_mday, time2->tm_hour,time2->tm_min);
 }
 
 //두 개의 파일 크기를 비교하는 함수 작성
